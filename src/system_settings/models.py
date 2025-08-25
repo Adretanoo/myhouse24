e5ac1ff184b9ab35478f67198381467b320e1714
Adretanoo <adriantegzza@gmail.com>
2025-08-25 10:18:52 +0300

    wrote models for the system tuning

---
+++ src/system_settings/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+
+class TranslationType(models.TextChoices):
+    INCOME = "income", "Приход"
+    EXPENSE = "expense", "Расход"
+
+
+class JobTitle(models.TextChoices):
+    DIRECTOR = "director", "Директор"
+    MANAGER = "manager", "Управляющий"
+    ACCOUNTANT = "accountant", "Бухгалтер"
+    ELECTRICIAN = "electrician", "Электрик"
+    PLUMBER = "plumber", "Сантехник"
+
+
+class UnitsMeasurement(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class ServiceSettings(models.Model):
+    title = models.CharField(max_length=255)
+    units_measurement = models.ForeignKey(
+        UnitsMeasurement, on_delete=models.CASCADE, default=JobTitle.MANAGER
+    )
+
+
+class TariffsSettings(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    edit_date = models.DateTimeField(auto_now=True)
+
+
+class PaymentDetailsSettings(models.Model):
+    title = models.CharField(max_length=255)
+    information = models.TextField()
+
+
+class PaymentDetailSettings(models.Model):
+    title = models.CharField(max_length=255)
+    translation_type = models.CharField(
+        choices=TranslationType.choices, max_length=10, default=TranslationType.INCOME
+    )
+
+
+class Roles(models.Model):
+    has_statistics = models.BooleanField(default=False)
+    has_cash_desk = models.BooleanField(default=False)
+    has_receipt = models.BooleanField(default=False)
+    has_personal_accounts = models.BooleanField(default=False)
+    has_apartments = models.BooleanField(default=False)
+    has_owner = models.BooleanField(default=False)
+    has_houses = models.BooleanField(default=False)
+    has_message = models.BooleanField(default=False)
+    has_request = models.BooleanField(default=False)
+    has_counters = models.BooleanField(default=False)
+    has_site_management = models.BooleanField(default=False)
+    has_services = models.BooleanField(default=False)
+    has_tariffs = models.BooleanField(default=False)
+    has_job_title = models.BooleanField(default=False)
+    has_users = models.BooleanField(default=False)
+    has_payment_details = models.BooleanField(default=False)
+    job_title = models.CharField(choices=JobTitle.choices, max_length=20)
